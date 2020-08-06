f473480b237f7813e5e307e627fbcbf694134131
GabrielCruzSoto <Gabrielcrsoto@outlook.com>
2020-08-06 00:48:23 -0400

    version 0.0.6
    Correccion server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/fr-pgs'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/fr-pgs/'}),
 );
 app.listen(process.env.PORT || 8080);
